8a2efaeb5082f43633041ccd83ae4e94e592e744
Radizzt <Richardthienhuynh@gmail.com>
2017-01-03 09:34:14 -0500

    Syntax template.

---
+++ js/syntax.js
@@ -0,0 +1,16 @@
+
+var vm = new Vue({
+   el: ".attrib",
+    data: {
+        dynamicId : "weee",
+        att:"attribute",
+        rawHtml: "raw"
+    }
+});
+
+var app8 = new Vue({
+    el: "#app-8",
+   data: {
+       ok : false
+   }
+});
