97bbdf91e635c1c77a101549d26e8ffe62093c73
Radizzt <Richardthienhuynh@gmail.com>
2017-01-03 14:15:46 -0500

    Syntax Directive complete.

--- js/syntax.js
+++ js/syntax.js
@@ -36,3 +36,67 @@ var syn3 = new Vue({
        ok : "something"
    }
 });
+
+//Directive
+var syn4 = new Vue({
+   el: "#syntax-4",
+    data: {
+        seen: true
+    }
+});
+
+//arguments
+var syn5 = new Vue({
+   el: "#syntax-5",
+    data:{
+        url: "http://google.ca",
+        name: "name of something"
+    },
+    methods: {
+        doSomething: function(e){
+            e.preventDefault();
+            alert("name value is: " + this.name);
+        }
+    }
+});
+
+//modifier
+var form1 = new Vue({
+    el:"#form1",
+    methods:{
+        onSubmit:function(e){
+            console.log("form submit");
+        }
+    }
+});
+
+//filter
+new Vue({
+    el: "#syntax-6",
+    data:{
+        message:"this is a message",
+        rawId: "rawID"
+    },
+    filters: {
+        capitalize: function (value) {
+            if (!value) return '';
+            value = value.toString();
+            return value.charAt(0).toUpperCase() + value.slice(1)
+        },
+        lastCap: function(value){
+            if (!value) return '';
+            value = value.toString();
+            return value.slice(0, value.length) + value.charAt(value.length - 1).toUpperCase();
+        },
+        //first argument is the initial value of the filter (message in thsi case)
+        foo: function(value, str2, str3){
+            console.log(value);
+            console.log(str2);
+            console.log(str3);
+            return " " + value + " " +  str2 + " " + str3;
+        }
+    }
+});
+
+
+
