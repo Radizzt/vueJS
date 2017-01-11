634435f5d140b09a5febfefa5c14f926cd1b144d
Radizzt <Richardthienhuynh@gmail.com>
2017-01-11 13:22:08 -0500

    Component: Custom Event Completed.

--- js/components.js
+++ js/components.js
@@ -2,7 +2,7 @@
 
 //register
 Vue.component('my-component', {
-   template: "<div>A custom component!</div>"
+    template: "<div>A custom component!</div>"
 });
 
 var Child = {
@@ -36,7 +36,7 @@ Vue.component("simple-counter",{
 
 //create a root instance
 new Vue({
-   el: '#component1',
+    el: '#component1',
     components:{
         // <my-component> will only be available in parent's template
         "my-component2": Child
@@ -60,7 +60,7 @@ Vue.component("child2",{
 });
 
 Vue.component("sum", {
-   props: ["num"],
+    props: ["num"],
     template: "<p> {{num + 1}}</p>"
 });
 
@@ -116,7 +116,7 @@ Vue.component('propValidation', {
 new Vue({
     el: "#component2",
     data:{
-      parentMsg: '',
+        parentMsg: '',
         inputNum:'',
         object:{
             notDefault: "not Default Message"
@@ -130,5 +130,112 @@ new Vue({
 
 });
 
+/*Custom Events*/
+Vue.component('button-counter', {
+    template: '<button v-on:click="increment">{{ counter }}</button>',
+    data: function () {
+        return {
+            counter: 0
+        }
+    },
+    methods: {
+        increment: function () {
+            this.counter += 1;
+            //emit the event, so the parent updates
+            this.$emit('domincrement');
+        }
+    }
+});
 
+
+new Vue({
+    el: '#component3',
+    data: {
+        total: 0
+    },
+    methods: {
+        incrementTotal: function () {
+            this.total += 1;
+        }
+    }
+});
+
+Vue.component('currency-input', {
+    template: '\
+    <div>\
+      <label v-if="label">{{ label }}</label>\
+      $\
+      <!-- ref is used to register a reference to an element or a child component. -->\
+      <!-- updateValue() is trigger on each input. -->\
+      <!-- focus trigger when an input box is focused (for safari bug only). -->\
+      <!-- blur trigger when an input box is not focus. -->\
+      <input\
+        ref="input"\
+        v-bind:value="value"\
+        v-on:input="updateValue($event.target.value)"\
+        v-on:focus="selectAll"\
+        v-on:blur="formatValue"\
+      >\
+    </div>\
+  ',
+    props: {
+        value: {
+            type: Number,
+            default: 0
+        },
+        label: {
+            type: String,
+            default: ''
+        }
+    },
+    //hook that is called after the instance has just been created, like a constructor everytime this component is call in the DOM
+    mounted: function () {
+        this.formatValue()
+    },
+    methods: {
+        updateValue: function (value) {
+            var result = currencyValidator.parse(value, this.value);
+            //if there's a warning from the validator. the current value will go back to what it was before the warning
+            if (result.warning) {
+                this.$refs.input.value = result.value;
+            }
+            //emit the event on the current instance, need it to update the value
+            this.$emit('input', result.value);
+        },
+        //format the value (put '.00' if there isn't one
+        formatValue: function () {
+            this.$refs.input.value = currencyValidator.format(this.value);
+        },
+        //if you're not using safari, you dont need this method
+        selectAll: function (event) {
+            // Workaround for Safari bug
+            // http://stackoverflow.com/questions/1269722/selecting-text-on-focus-using-jquery-not-working-in-safari-and-chrome
+            setTimeout(function () {
+                event.target.select();
+            }, 0);
+        }
+    }
+});
+
+var bus = new Vue();
+
+new Vue({
+    el: '#component4',
+    data: {
+        price: 0,
+        shipping: 0,
+        handling: 0,
+        discount: 0
+    },
+    computed: {
+        total: function () {
+            return ((
+                this.price * 100 +
+                this.shipping * 100 +
+                this.handling * 100 -
+                this.discount * 100
+            ) / 100).toFixed(2);
+        }
+    }
+});
 
