2a9e8533e67120cefa848d443586b4e99379aa7d
Radizzt <Richardthienhuynh@gmail.com>
2017-01-11 10:12:12 -0500

    Components: Props completed.

--- js/components.js
+++ js/components.js
@@ -43,6 +43,92 @@ new Vue({
     }
 });
 
+/*properties*/
+Vue.component('child', {
+    // declare the props
+    // camelCase Props MUST be in kebab-case on the html file
+    props: ['message', "message2", "myMessage"],
+    // just like data, the prop can be used inside templates
+    // and is also made available in the vm as this.message
+    template: '<p>message: {{ message }}, <br> message2: {{message2}}, <br> myMessage: {{myMessage}}</p>'
+});
+
+Vue.component("child2",{
+    props: ["myMessage"],
+    template: "<p>myMessage: {{myMessage}}</p>"
+
+});
+
+Vue.component("sum", {
+   props: ["num"],
+    template: "<p> {{num + 1}}</p>"
+});
+
+var Child2 = {
+    template: "<child2 myMessage='test'></child2>"
+};
+
+//Instead of defining the props as an array of strings, you can use an object with validation requirements:
+
+Vue.component('propValidation', {
+    props: {
+        // basic type check (`null` means accept any type)
+        propA: Number,
+        // multiple possible types
+        propB: String,
+        // a required string
+        propC: {
+            type: String,
+            required: true
+        },
+        // a number with default value
+        propD: {
+            type: Number,
+            default: 100
+        },
+        // object/array defaults should be returned from a
+        // factory function
+        propE: {
+            type: Object,
+            default: function () {
+                return { message: 'hello' }
+            }
+        },
+        // custom validator function
+        propF: {
+            type: Number,
+            validator: function (value) {
+                console.log("inside validator: " + value);
+                return value > 10;
+            }
+        }
+    },
+    template:"<div>" +
+    "<p>PropA (Number): {{propA}}</p>" +
+    "<p>PropB ([String, Number]): {{propB}}</p>" +
+    "<p>PropC (Require String): {{propC}}</p>" +
+    "<p>PropD (Default Number): {{propD}}</p>" +
+    "<p>PropE (Default Object/Array): {{propE}}</p>" +
+    "<p>PropF (Custom Validator): {{propF}} </p>" +
+    "</div>"
+});
+
+new Vue({
+    el: "#component2",
+    data:{
+      parentMsg: '',
+        inputNum:'',
+        object:{
+            notDefault: "not Default Message"
+        },
+        test:'test'
+    },
+    components:{
+        //using string template to use 'myMessage' instead of my-message
+        children : Child2
+    }
+
+});
 
 
 
